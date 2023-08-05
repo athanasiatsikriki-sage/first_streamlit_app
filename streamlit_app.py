524de20fd89d87cddd3b0ff40a515d08f072b271
Athanasia Tsikriki <athanasia.tsikriki@sage.com>
2023-08-05 12:33:52 +0100

    Update streamlit_app.py
    streamlit.header

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header('Breakfast Favorites')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
